b3d4630f91b8f19ead1299d6bb5a2f6811384f32
adcanada <64662897+adcanada@users.noreply.github.com>
2024-10-19 14:07:50 -0400

    put back only not broken stuff

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,15 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <div className="not-found-container">
+        <h1>Oops! This page is missing...</h1>
+        <p>It seems the page you're looking for has disappeared or never existed!</p>
+        <div className="cat-gif">
+          <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" alt="Funny Cat" />
+        </div>
+        <p>But don't worry, here's a cat to cheer you up. Why not head back to the homepage?</p>
+        <a href="/" className="home-button">Go to Homepage</a>
+      </div>
     </Layout>
   );
 }
